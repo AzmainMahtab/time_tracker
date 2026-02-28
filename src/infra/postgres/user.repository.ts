95f781cbb51010a34deef6807c4be055659f9d25
Azmain Mahtab <azmainmahtab97@gmail.com>
2026-02-28 14:40:41 +0600

    repo update
    for got to impliment depandency injection and now fixed it
    should be more testable with test containers

--- src/infra/postgres/user.repository.ts
+++ src/infra/postgres/user.repository.ts
@@ -1,11 +1,21 @@
-import * as db from './pool.js'
+import * as defaultPool from './pool.js';
 import { IUserRepository } from '../../ports/user.ports.js';
 import { User } from '../../domains/user.domain.js';
 
+//interface for bot pg client and pool acceptance 
+interface IDatabase {
+  query: (text: string, params?: any[]) => Promise<any>;
+}
 
 export class PostgresUserRepository implements IUserRepository {
+  // We store the connection here
+  private readonly db: IDatabase;
 
-  //Save a user
+  // If no db is provided, it defaults to the production pool.
+  constructor(db: IDatabase = defaultPool) {
+    this.db = db;
+  }
+
   async save(user: User): Promise<User> {
     const sql = `
       INSERT INTO users (email, password_hash, full_name)
@@ -19,58 +29,40 @@ export class PostgresUserRepository implements IUserRepository {
         updated_at AS "updatedAt";
     `;
 
-    const values = [
-      user.email,
-      user.passwordHash,
-      user.fullName
-    ];
+    const values = [user.email, user.passwordHash, user.fullName];
 
-    // using pool's query method with parameter binding to prevent sql injection
-    const { rows } = await db.query(sql, values);
+    const { rows } = await this.db.query(sql, values);
 
-    // Return a new User domain 
     return new User(rows[0]);
   }
 
-  //find a user by email
   async findByEmail(email: string): Promise<User | null> {
     const sql = `
       SELECT 
-        uuid, 
-        email, 
-        password_hash AS "passwordHash", 
-        full_name AS "fullName", 
-        created_at AS "createdAt", 
+        uuid, email, password_hash AS "passwordHash", 
+        full_name AS "fullName", created_at AS "createdAt", 
         updated_at AS "updatedAt"
       FROM users 
       WHERE email = $1;
     `;
 
-    const { rows } = await db.query(sql, [email]);
-
+    const { rows } = await this.db.query(sql, [email]);
     if (rows.length === 0) return null;
-
     return new User(rows[0]);
   }
 
-  //find a user by the uuid
   async findById(uuid: string): Promise<User | null> {
     const sql = `
       SELECT 
-        uuid, 
-        email, 
-        password_hash AS "passwordHash", 
-        full_name AS "fullName", 
-        created_at AS "createdAt", 
+        uuid, email, password_hash AS "passwordHash", 
+        full_name AS "fullName", created_at AS "createdAt", 
         updated_at AS "updatedAt"
       FROM users 
       WHERE uuid = $1;
     `;
 
-    const { rows } = await db.query(sql, [uuid]);
-
+    const { rows } = await this.db.query(sql, [uuid]);
     if (rows.length === 0) return null;
-
     return new User(rows[0]);
   }
 }
