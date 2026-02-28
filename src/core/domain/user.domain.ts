fd1bf3685cb5528fb091217d8a9b3a9dc55ae4f4
Azmain Mahtab <azmainmahtab97@gmail.com>
2026-02-28 13:39:22 +0600

    User domain

---
+++ src/core/domain/user.domain.ts
@@ -0,0 +1,26 @@
+export interface UserProps {
+  id?: string;
+  email: string;
+  passwordHash: string;
+  fullName?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export class User {
+  public readonly id?: string;
+  public readonly email: string;
+  public readonly passwordHash: string;
+  public readonly fullName?: string;
+  public readonly createdAt?: Date;
+  public readonly updatedAt?: Date;
+
+  constructor(props: UserProps) {
+    this.id = props.id;
+    this.email = props.email;
+    this.passwordHash = props.passwordHash;
+    this.fullName = props.fullName;
+    this.createdAt = props.createdAt;
+    this.updatedAt = props.updatedAt;
+  }
+}
